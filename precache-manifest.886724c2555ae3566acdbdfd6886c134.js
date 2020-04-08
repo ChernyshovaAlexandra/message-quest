1509ffb530a74660098b7032996dabb7ef33baa3
Александра Чернышева <ach@worldfashion.pro>
2020-04-08 14:17:14 +0300

    Updates

---
+++ precache-manifest.886724c2555ae3566acdbdfd6886c134.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7270c04d096f7d49a9c44cb3c3ec28ab",
+    "url": "/message-quest/index.html"
+  },
+  {
+    "revision": "6ec0bfcd25c7614f3d66",
+    "url": "/message-quest/static/css/2.4fc5a6bc.chunk.css"
+  },
+  {
+    "revision": "d841ac09999f024286ca",
+    "url": "/message-quest/static/css/main.791c47f5.chunk.css"
+  },
+  {
+    "revision": "6ec0bfcd25c7614f3d66",
+    "url": "/message-quest/static/js/2.e5e09cb5.chunk.js"
+  },
+  {
+    "revision": "dc852af523300ac19b39039607750698",
+    "url": "/message-quest/static/js/2.e5e09cb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d841ac09999f024286ca",
+    "url": "/message-quest/static/js/main.aabc6279.chunk.js"
+  },
+  {
+    "revision": "54d1895f5e471d8bea37",
+    "url": "/message-quest/static/js/runtime-main.d0a375ef.js"
+  },
+  {
+    "revision": "4fa0d1c2bab42768e94e69f101b1972d",
+    "url": "/message-quest/static/media/bg.4fa0d1c2.jpg"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/message-quest/static/media/persik.4e1ec840.png"
+  }
+]);
